bd6f20202accd15230dc2a42daf5297ef276ba30
Stanislav Zeman <zeman@standa.dev>
2025-04-12 22:41:26 +0200

    feat(routes): draft pages

---
+++ src/routes/languages/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Grid from "$lib/components/Grid.svelte";
+
+    const columns = [
+        { headerName: 'ID', field: 'id' },
+        { headerName: 'Name', field: 'name' },
+        { headerName: 'Plural forms', field: 'pluralForms' },
+        { headerName: 'Plural formula', field: 'pluralFormula' },
+    ];
+
+    const rows = [
+        {
+            id: 'cs',
+            name: 'Czech',
+            pluralForms: 3,
+            pluralFormula: "N == 1 ? 0 : ((N > 1 && N < 5) ? 1 : 2)",
+        },
+        {
+            id: 'sk',
+            name: 'Slovak',
+            pluralForms: 3,
+            pluralFormula: "N == 1 ? 0 : ((N > 1 && N < 5) ? 1 : 2)",
+        },
+        {
+            id: 'en',
+            name: 'English',
+            pluralForms: 2,
+            pluralFormula: "N>1 ? 1 : 0",
+        },
+    ];
+</script>
+
+<h1>Languages</h1>
+
+<Grid columnDefs={columns} rowData={rows} />
