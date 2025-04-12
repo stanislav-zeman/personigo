a861e31ea6abb2d86b6805c4765448214785975d
Stanislav Zeman <zeman@standa.dev>
2025-04-12 22:40:47 +0200

    feat(components): create simple data grid from ag grid

---
+++ src/lib/components/data/Grid.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" generics="TRow">
+    import { createGrid, ModuleRegistry, AllCommunityModule} from 'ag-grid-community';
+    import type { ColDef, GridOptions } from 'ag-grid-community';
+	import { onMount } from 'svelte';
+
+    ModuleRegistry.registerModules([AllCommunityModule]);
+
+    let gridDiv: HTMLDivElement;
+
+    let {
+        columnDefs,
+        rowData,
+    }: {
+        columnDefs: Array<ColDef> ;
+        rowData: Array<TRow> ;
+    } = $props();
+
+
+    const gridOptions: GridOptions<TRow> = {
+            columnDefs,
+            rowData,
+            rowSelection: {
+                mode: "multiRow",
+            },
+            defaultColDef: {
+                sortable: true,
+                filter: true
+            }
+        };
+
+    onMount(() => {
+        createGrid(gridDiv, gridOptions);
+    })
+</script>
+
+<div bind:this={gridDiv} style="height: 400px; width: 80%;"></div>
