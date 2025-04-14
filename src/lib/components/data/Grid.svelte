5e759a60b978de8bfda4a1f249972cd574c626a7
Stanislav Zeman <zeman@standa.dev>
2025-04-14 02:57:01 +0200

    feat(components): add autosize to data grid

--- src/lib/components/data/Grid.svelte
+++ src/lib/components/data/Grid.svelte
@@ -1,37 +1,39 @@
 <script lang="ts" generics="TRow">
-    import { createGrid, ModuleRegistry, AllCommunityModule} from 'ag-grid-community';
-    import type { ColDef, GridOptions } from 'ag-grid-community';
+	import { createGrid, ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+	import type { ColDef, GridOptions } from 'ag-grid-community';
 	import { AllEnterpriseModule } from 'ag-grid-enterprise';
 	import { onMount } from 'svelte';
 
-    ModuleRegistry.registerModules([AllCommunityModule, AllEnterpriseModule]);
+	ModuleRegistry.registerModules([AllCommunityModule, AllEnterpriseModule]);
 
-    let gridDiv: HTMLDivElement;
+	let gridDiv: HTMLDivElement;
 
-    let {
-        columnDefs,
-        rowData,
-    }: {
-        columnDefs: Array<ColDef> ;
-        rowData: Array<TRow> ;
-    } = $props();
+	let {
+		columnDefs,
+		rowData
+	}: {
+		columnDefs: Array<ColDef>;
+		rowData: Array<TRow>;
+	} = $props();
 
+	const gridOptions: GridOptions<TRow> = {
+		columnDefs,
+		rowData,
+		rowSelection: {
+			mode: 'multiRow'
+		},
+		defaultColDef: {
+			sortable: true,
+			filter: true
+		},
+		autoSizeStrategy: {
+			type: 'fitCellContents'
+		}
+	};
 
-    const gridOptions: GridOptions<TRow> = {
-            columnDefs,
-            rowData,
-            rowSelection: {
-                mode: "multiRow",
-            },
-            defaultColDef: {
-                sortable: true,
-                filter: true
-            }
-        };
-
-    onMount(() => {
-        createGrid(gridDiv, gridOptions);
-    })
+	onMount(() => {
+		createGrid(gridDiv, gridOptions);
+	});
 </script>
 
-<div bind:this={gridDiv} style="height: 400px; width: 80%;"></div>
+<div bind:this={gridDiv} class="m-6 h-svh"></div>
