d7d8104e633456fbb90ea12989d73651c8c36160
Stanislav Zeman <zeman@standa.dev>
2025-04-14 02:50:33 +0200

    feat(components): add navbar to navigation

---
+++ src/lib/components/navigation/Menu.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { Icon, Home, Languages, FolderCog, Folder, BookA, CaseSensitive } from 'lucide-svelte';
+
+	type Item = {
+		name: string;
+		path: string;
+		icon: typeof Icon;
+	};
+
+	const items: Item[] = [
+		{
+			name: 'Home',
+			path: '/',
+			icon: Home
+		},
+		{
+			name: 'Installations',
+			path: '/installations',
+			icon: Folder
+		},
+		{
+			name: 'Customizations',
+			path: '/customizations',
+			icon: FolderCog
+		},
+		{
+			name: 'Languages',
+			path: '/languages',
+			icon: Languages
+		},
+		{
+			name: 'Phrases',
+			path: '/phrases',
+			icon: CaseSensitive
+		},
+		{
+			name: 'Translations',
+			path: '/translations',
+			icon: BookA
+		}
+	];
+</script>
+
+<nav class="flex flex-1 flex-col">
+	<ul class="menu">
+		{#each items as item (item.name)}
+			{@const Icon = item.icon}
+			<li>
+				<a href={item.path}>
+					<Icon />
+					<span>{item.name}</span>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</nav>
---
+++ src/lib/components/navigation/Navbar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { ChevronLeft, ChevronRight, Moon, Settings, Sun } from 'lucide-svelte';
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+	<label class="btn btn-circle swap swap-rotate">
+		<input type="checkbox" />
+		<ChevronRight class="swap-off" />
+		<ChevronLeft class="swap-on" />
+	</label>
+	<div class="flex-1">
+		<p class="mx-2 text-xl">Personigo</p>
+	</div>
+	<div>
+		<label class="flex cursor-pointer gap-2">
+			<Moon />
+			<input type="checkbox" value="emerald" class="toggle theme-controller" />
+			<Sun />
+		</label>
+	</div>
+	<div class="flex-none pl-8">
+		<button class="btn btn-square btn-ghost">
+			<a href="/settings">
+				<Settings />
+			</a>
+		</button>
+	</div>
+</div>
