71d67bf30cc0eeb3e808103b7e5d0b13932b4b2e
Stanislav Zeman <zeman@standa.dev>
2025-04-14 02:57:30 +0200

    feat(routes): rework layout to include navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import '../app.css';
-    import Navigation from '$lib/components/navigation/Navigation.svelte'
+	import Menu from '$lib/components/navigation/Menu.svelte';
+	import Navbar from '$lib/components/navigation/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
-<div class="flex">
-    <Navigation/>
-    <div class="flex-6">
-        {@render children()}
-    </div>
+<div class="flex flex-col">
+	<Navbar />
+	<div class="flex flex-row">
+		<Menu />
+		<div class="flex-6">
+			{@render children()}
+		</div>
+	</div>
 </div>
