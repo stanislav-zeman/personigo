b20ca513ab890342eac6cef36acaa3d65ccefa9f
Stanislav Zeman <zeman@standa.dev>
2025-05-03 16:27:50 +0200

    feat(lib): create file for application constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+const contentTypeJsonHeader = {
+	'Content-Type': 'application/json; charset=UTF-8'
+};
+
+export { contentTypeJsonHeader };
+
+const apiUrl = 'http://localhost:1239';
+
+export { apiUrl };
+
+const authTokenName = 'token';
+
+export { authTokenName };
