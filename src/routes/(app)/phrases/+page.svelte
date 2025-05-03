8baa58179689fd2963c319409709b45cfde2fe33
Stanislav Zeman <zeman@standa.dev>
2025-05-03 17:14:46 +0200

    feat(routes): implement phrases

---
+++ src/routes/(app)/phrases/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+	import Grid from '$lib/components/data/Grid.svelte';
+
+	let { data }: PageProps = $props();
+
+	const columns = [
+		{ headerName: 'ID', field: 'id' },
+		{ headerName: 'Phrase', field: 'phrase' }
+	];
+
+	console.log(data.rows);
+	const rows = data.rows;
+</script>
+
+<Grid columnDefs={columns} rowData={rows} />
